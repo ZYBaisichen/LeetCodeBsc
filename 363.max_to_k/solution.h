0468a7eb3c9d9b32fddc196168e30911b75daa89
baisichen <baisichen@baidu.com>
2021-04-22 11:44:07 +0800

    完整解题思路

--- 363.max_to_k/solution.h
+++ 363.max_to_k/solution.h
@@ -1,7 +1,7 @@
 /*** 
  * @Author: baisichen
  * @Date: 2021-04-22 10:58:26
- * @LastEditTime: 2021-04-22 11:10:07
+ * @LastEditTime: 2021-04-22 11:39:43
  * @LastEditors: baisichen
  * @Description: 
  */
@@ -12,26 +12,27 @@ using namespace std;
 class Solution {
 public:
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
-        int ans = INT_MIN;
+        int res = INT_MIN;
         int m = matrix.size(), n = matrix[0].size();
         for (int i = 0; i < m; ++i) { // 枚举上边界
             vector<int> sum(n);
-            for (int j = i; j < m; ++j) { // 枚举下边界
-                for (int c = 0; c < n; ++c) {
-                    sum[c] += matrix[j][c]; // 更新每列的元素和
+            for (int ii = i; ii < m; ++ii) { // 枚举下边界
+                for (int j = 0; j < n; ++j) {
+                    sum[j] += matrix[ii][j]; // 更新每列的元素和
                 }
-                set<int> sumSet{0};
-                int s = 0;
+                set<int> s_r_i{0};
+                int s_r = 0;
                 for (int v : sum) {
-                    s += v;
-                    auto lb = sumSet.lower_bound(s - k);
-                    if (lb != sumSet.end()) {
-                        ans = max(ans, s - *lb);
+                    s_r += v;
+                    //类似动态规划思想
+                    auto s_l = s_r_i.lower_bound(s_r - k);
+                    if (s_l != s_r_i.end()) {
+                        res = max(res, s_r - *s_l);
                     }
-                    sumSet.insert(s);
+                    s_r_i.insert(s_r);
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
