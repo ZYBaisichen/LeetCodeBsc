036be9c51b6361139cefa586db85355b1a4c2de1
ZYBaisichen <bhbaisichen@gmail.com>
2021-05-19 09:54:21 +0800

    add 正则表达式

--- 剑指offer/19.正则表达式匹配/solution.h
+++ 剑指offer/19.正则表达式匹配/solution.h
@@ -18,23 +18,25 @@ public:
     bool isMatch(string s, string p) {
         int m = s.length();
 		int n = p.length();
-		vector< vector<bool> > dp(m+1, vector<bool>(n+1, false));
+		vector< vector<bool> > dp(m + 1, vector<bool>(n + 1, false));
 		dp[0][0] = true;
-		for (int i=0;i<=m;i++) {
-			for (int j=1;j<=n;j++) {
-				if (p[j-1] == '.' || (i>=1 && p[j-1] == s[i-1])) {
+		for (int i = 0; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (p[j - 1] == '.' || (i >= 1 && p[j - 1] == s[i - 1])) {
 					//如果i为0，有实际字母或为点的模式是匹配不了该空串的
-					if (i>=1) {
-						dp[i][j] = dp[i-1][j-1];
+					if (i >= 1) {
+						dp[i][j] = dp[i - 1][j - 1];
 					}
-				} else if(j>=2 && p[j-1]=='*') {
-					if (s[i-1]==p[j-2] || p[j-2]=='.') {
-						dp[i][j] =  dp[i][j-2];
-						if (i>=1) { //如果i为0，则匹配的是一个空串，没有必要再往前看了
-							dp[i][j] = dp[i][j]||dp[i-1][j];
+				}
+				else if (j >= 2 && p[j - 1] == '*') {
+					if ((i>=1 && s[i - 1] == p[j - 2]) || p[j - 2] == '.') {
+						dp[i][j] = dp[i][j - 2];
+						if (i >= 1) { //如果i为0，则匹配的是一个空串，没有必要再往前看了
+							dp[i][j] = dp[i][j] || dp[i - 1][j];
 						}
-					} else {
-						dp[i][j] = dp[i][j-2];
+					}
+					else {
+						dp[i][j] = dp[i][j - 2];
 					}
 				}
 			}
