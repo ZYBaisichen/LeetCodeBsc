1c107fd0c560d0722f4f25b7e9c337c8ebd58e33
ZYBaisichen <bhbaisichen@gmail.com>
2021-06-11 00:23:12 +0800

    change

--- 剑指offer/56-I.数组中数字出现的次数/solution.h
+++ 剑指offer/56-I.数组中数字出现的次数/solution.h
@@ -10,6 +10,29 @@ using namespace std;
 class Solution {
 public:
     vector<int> singleNumbers(vector<int>& nums) {
-
+        int len = nums.size();
+        vector<int>  res;
+        if (len==0) return res;
+        
+        int tmp = 0;
+        for (int i=0;i<len;i++) {
+            tmp ^= nums[i];
+        }
+        int binary = 1;
+        while (!(binary & tmp)) {
+            binary <<= 1;
+        }
+        
+        int res1=0, res2 =0;
+        for (int i=0;i<len;i++) {
+            if (nums[i] & binary) {
+                res1 ^= nums[i];
+            } else {
+                res2 ^= nums[i];
+            }
+        }
+        res.push_back(res1);
+        res.push_back(res2);
+        return res;
     }
 };
