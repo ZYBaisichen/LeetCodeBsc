1459f7374decf3ffe46c9887229ba7bc3dbc5aae
ZYBaisichen <bhbaisichen@gmail.com>
2021-06-28 00:08:41 +0800

    add 127

--- 127.word-ladder/solution.h
+++ 127.word-ladder/solution.h
@@ -62,4 +62,82 @@ public:
 		}
 		return res;
 	}
+	void pushNew(string word, queue<string>& cur_que, queue<string>& other_que, unordered_map<string, int>& cur_level, unordered_map<string, int>& other_level, int& res, unordered_set<string>& wordSet) {
+		int len = word.length();
+		for (int i = 0; i<len; i++) {
+			int letter = 'a';
+			while (letter <= 'z') {
+				//产生新的单词
+				if (letter != word[i]) {
+					string tmp = word;
+					tmp[i] = letter;
+					if (wordSet.find(tmp) != wordSet.end()) {
+						if (other_level.find(tmp) != other_level.end()) {
+							//因为广度优先遍历，每次试着往前走一步，所以第一个碰到的结果即为最优解
+							res = other_level[tmp] + cur_level[word];
+                            return;
+						}
+						else if (cur_level.find(tmp) == cur_level.end()) {
+							cur_que.push(tmp);
+							cur_level[tmp] = cur_level[word] + 1;
+						}
+					}
+				}
+				letter++;
+			}
+		}
+	}
+	// 双向BFS
+	int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+		if (beginWord == endWord) {
+			return 1;
+		}
+		
+		unordered_set<string> wordSet(wordList.begin(), wordList.end());
+
+		if (beginWord.length() != endWord.length()
+			|| wordSet.find(endWord) == wordSet.end()) {
+			return 0;
+		}
+
+
+		queue<string> word_que1;//正向
+		unordered_map<string, int> level1;
+
+		queue<string> word_que2;//反向
+		unordered_map<string, int> level2;
+
+		//搜索开始节点
+		word_que1.push(beginWord);
+		level1[beginWord] = 1;
+
+		word_que2.push(endWord);
+		level2[endWord] = 1;
+
+		int res = 0;
+		//有一个搜索结束还没有碰到，说明没找到结果
+		while (!word_que1.empty() && !word_que2.empty()) {
+
+
+			string cur_word = "";
+			//搜索平均分摊
+			if (word_que1.size() <= word_que2.size()) {
+				cur_word = word_que1.front();
+				pushNew(cur_word, word_que1, word_que2, level1, level2, res, wordSet);
+				word_que1.pop();
+			}
+			else {
+				cur_word = word_que2.front();
+				pushNew(cur_word, word_que2, word_que1, level2, level1, res, wordSet);
+				word_que2.pop();
+			}
+
+			if (res != 0) {
+				break;
+			}
+		}
+
+
+		return res;
+	}
 };
