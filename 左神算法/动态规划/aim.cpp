6b0a818accb09add1316434c020bda623a6efec3
ZYBaisichen <bhbaisichen@gmail.com>
2023-10-17 12:06:21 +0800

    左神

---
+++ 左神算法/动态规划/aim.cpp
@@ -0,0 +1,20 @@
+/*** 
+ * @Author: baisichen
+ * @Date: 2023-10-12 16:54:25
+ * @LastEditTime: 2023-10-12 17:12:53
+ * @LastEditors: baisichen
+ * @Description: 汉诺塔问题, 
+ * 最优2^N-1， T(N) = T(N-1) + T(n-2) + .... + 1
+ */
+#include "iostream" 
+#include <vector>
+#include <stack>
+using namespace std;
+void process3(vector<int> arr, int aim) {
+    int N = arr.size();
+    int dp[N][aim] = 
+
+}
+int main() {
+    return 0;
+}
